b19005951f0e2efb09d967264f52521cb022f6fb
MakerSquare Student <student@challenge-4.att.net>
2013-06-20 14:13:35 -0500

    Added Favorite and Follow functionality

--- hailinator.rb
+++ hailinator.rb
@@ -4,13 +4,18 @@ require 'csv'
 Twitter.configure do |config|
   config.consumer_key = "S1dTtHshDW8STRhiHpg3UQ"
   config.consumer_secret = "zGaAm9OXTRcs1T6v82OkwfG6QVOusHWpdQUTHHI8o"
-  config.oauth_token = "1210807495-WFaEbH7gX2XSN526uCsumZdY3VTrUrPstOTCj34"
-  config.oauth_token_secret = "mX1JMl6YbOcvXbGLdyTK4BlrcKtWmAcj2cWG66zW2L4"
+  config.oauth_token = "1210807495-AiQtJExHEUOndQePa7racTkOGnyZk5zYJQ9cuj3"
+  config.oauth_token_secret = "3HE8EoRyTjC8rhchhEp7zYqCE1fAsMrs0p7PgXtXek"
 end
+
 
 CSV.open('hailinator.csv', 'wb') do |csv|
 	csv << ["handle", "text", "url"]
   Twitter.search('#haildamage').results.map do |status|
     csv << ["#{status.from_user}", "#{status.text}", "twitter.com/#{status.from_user}/statuses/#{status.id}"]
+    	#  This junk WORKS!
+    # Twitter.favorite(status.id)
+    # Twitter.follow("#{status.from_user}")
+
   end
 end
